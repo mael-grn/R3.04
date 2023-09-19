f177250d4edbcb8f010363e3796c093a11a23d1a
garnmael <maelgarnier5@gmail.com>
2023-09-19 11:40:14 +0200

    template de l'ex 3 terminée

--- TP02/exercice3/ObjetContraint.h
+++ TP02/exercice3/ObjetContraint.h
@@ -7,6 +7,18 @@ class ObjetContraint {
 
     // A COMPLETER : Spécifiez ici le template de classe ObjetContraint<T>
 
+public:
+    ObjetContraint(T val, T min, T max);
+    const T& getMin() const;
+    const T& getMax() const;
+    const T& getMval() const;
+    void setVal(const T& val);
+    operator T() const;
+
+private:
+    T val;
+    T min;
+    T max;
 };
 
 // A COMPLETER : Implémentez ici les méthodes du template ObjetContraint<T> (pas de fichier .cpp pour un template)
