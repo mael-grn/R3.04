88943a92d9d36b79e0b5ab53e2c2ded062122f5e
garnmael <maelgarnier5@gmail.com>
2023-09-26 10:11:19 +0200

    ajout du sujet de tp

---
+++ TP03/exercice1/Visage.cpp
@@ -0,0 +1,32 @@
+#include "Visage.h"
+#include <iostream>
+using namespace std;
+
+// A COMPLETER : IMPLEMENTER LES METHODES DE LA CLASSE VISAGE (ET L'OPERATEUR <<)
+
+Visage::Visage(int longueurNez, const Ethnie &uneEthnie) {
+}
+
+void Visage::setMoustache(int largeur) {
+}
+
+void Visage::addBouton(int diametre) {
+}
+
+void Visage::setChapeau(const Chapeau &unChapeau) {
+}
+
+void Visage::addBijou(const Bijou &unBijou) {
+}
+
+const Visage &Visage::operator=(const Visage &unVisage) {
+}
+
+Visage::Visage(const Visage &unVisage) {
+}
+
+Visage::~Visage() {
+}
+
+std::ostream &operator<<(std::ostream &sortie, const Visage &visage) {
+}
---
+++ TP03/exercice1/Visage.h
@@ -0,0 +1,31 @@
+#ifndef VISAGE_H
+#define VISAGE_H
+
+#include "AttributsVisage.h"
+#include <vector>
+
+// A COMPLETER (spécifiez les attributs)
+
+class Visage {
+public:
+    Visage(int longueurNez, const Ethnie &uneEthnie);
+    void setMoustache(int largeur);
+    void addBouton(int diametre);
+    void setChapeau(const Chapeau &unChapeau);
+    void addBijou(const Bijou &unBijou);
+    const Visage &operator=(const Visage &unVisage);
+    Visage(const Visage &unVisage);
+    ~Visage();
+
+    // fonction amie : aura accès aux attributs "private"  de la classe Visage (pour éviter d'écrire les getters)
+    // attention ce n'est pas une méthode !
+    friend std::ostream &operator<<(std::ostream &sortie, const Visage &visage);
+
+private:
+
+    // A COMPLETER : CODEZ CORRECTEMENT LES COMPOSITIONS ET AGREGATIONS
+
+};
+
+#endif /* VISAGE_H */
+
